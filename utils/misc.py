38467f89dfb6d4ec1cc3a0be7f06bbf35716e4fd
Hoel Bagard <hoel.bagard@gmail.com>
2021-02-10 12:23:19 +0900

    Added misc utility file

---
+++ utils/misc.py
@@ -0,0 +1,15 @@
+from shutil import get_terminal_size
+from typing import (
+    Optional,
+    Tuple
+)
+
+
+def clean_print(msg: str, fallback: Optional[Tuple[int, int]] = (156, 38)):
+    """
+    Function that prints the given string to the console and erases any previous print made on the same line
+    Args:
+        msg: String to print to the console
+        fallback: Size of the terminal to use if it cannot be determined by shutil (if using windows for example)
+    """
+    print(msg + ' ' * (get_terminal_size(fallback=fallback.columns - len(msg))))
