0b334c6e1591d53af08b2e351d88df9f26a3debd
Hoel Bagard <hoel.bagard@gmail.com>
2021-02-11 18:49:24 +0900

    Added end to clean_print

--- utils/misc.py
+++ utils/misc.py
@@ -5,11 +5,11 @@ from typing import (
 )
 
 
-def clean_print(msg: str, fallback: Optional[Tuple[int, int]] = (156, 38)):
+def clean_print(msg: str, fallback: Optional[Tuple[int, int]] = (156, 38), end='\n'):
     """
     Function that prints the given string to the console and erases any previous print made on the same line
     Args:
         msg: String to print to the console
         fallback: Size of the terminal to use if it cannot be determined by shutil (if using windows for example)
     """
-    print(msg + ' ' * (get_terminal_size(fallback=fallback.columns - len(msg))))
+    print(msg + ' ' * (get_terminal_size(fallback=fallback).columns - len(msg)), end=end)
