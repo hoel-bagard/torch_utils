9e7e099dd0bb21cda4d9377cd08f3cba43912ee5
Hoel Bagard <hoel.bagard@gmail.com>
2022-02-22 10:46:30 +0900

    Removed the drawing fn for segmentation.

--- utils/draw.py
+++ utils/draw.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional
+from typing import Optional
 
 import cv2
 import numpy as np
@@ -149,58 +149,3 @@ def draw_pred_video(video_tensor: torch.Tensor,
         new_video = np.expand_dims(new_video, -1)
 
     return new_video
-
-
-def draw_segmentation(input_imgs: torch.Tensor,
-                      one_hot_masks_preds: torch.Tensor,
-                      one_hot_masks_labels: torch.Tensor,
-                      color_map: list[tuple[int, int, int]],
-                      denormalize_img_fn: Optional[Callable[[np.ndarray], np.ndarray]] = None,
-                      size: Optional[tuple[int, int]] = None) -> np.ndarray:
-    """Recreate the segmentation masks from their one hot representations, and place them next to the original image.
-
-    Args:
-        input_imgs (torch.Tensor): Images that were fed to the network.
-        one_hot_masks_labels (torch.Tensor): One hot representation of the label segmentation masks.
-        one_hot_masks_preds (torch.Tensor): One hot representation of the prediction segmentation masks.
-        color_map (list): List linking class index to its color
-        denormalize_img_fn (callable): Function to reverse the normalization process.
-        size (int, optional): If given, the images will be resized to this size
-
-    Returns:
-        np.ndarray: RGB segmentation masks and original image (in one image)
-    """
-    imgs = rearrange(input_imgs, "b c w h -> b w h c").cpu().detach().numpy()
-    if denormalize_img_fn is not None:
-        imgs = denormalize_img_fn(imgs).astype(np.uint8)
-
-    one_hot_masks_preds = rearrange(one_hot_masks_preds, "b c w h -> b w h c")
-    masks_preds: np.ndarray = torch.argmax(one_hot_masks_preds, dim=-1).cpu().detach().numpy()
-    one_hot_masks_labels = rearrange(one_hot_masks_labels, "b c w h -> b w h c")
-    masks_labels: np.ndarray = torch.argmax(one_hot_masks_labels, dim=-1).cpu().detach().numpy()
-
-    width, height, _ = imgs[0].shape  # All images are expected to have the same shape
-
-    # Create a blank image with some text to explain what things are
-    text_img = np.full((width, height, 3), 255, dtype=np.uint8)
-    text_img = cv2.putText(text_img, "Top left: input image.", (20, 20),
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1, cv2.LINE_AA)
-    text_img = cv2.putText(text_img, "Top right: label mask", (20, 40),
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1, cv2.LINE_AA)
-    text_img = cv2.putText(text_img, "Bottom left: predicted mask", (20, 60),
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1, cv2.LINE_AA)
-
-    out_imgs = []
-    for img, pred_mask, label_mask in zip(imgs, masks_preds, masks_labels):
-        # Recreate the segmentation mask from its one hot representation
-        pred_mask_rgb = np.asarray(color_map[pred_mask], dtype=np.uint8)
-        label_mask_rgb = np.asarray(color_map[label_mask], dtype=np.uint8)
-
-        out_img_top = cv2.hconcat((img, label_mask_rgb))
-        out_img_bot = cv2.hconcat((pred_mask_rgb, text_img))
-        out_img = cv2.vconcat((out_img_top, out_img_bot))
-        if size:
-            out_img = cv2.resize(out_img, size, interpolation=cv2.INTER_AREA)
-        out_imgs.append(out_img)
-
-    return np.asarray(out_imgs)
