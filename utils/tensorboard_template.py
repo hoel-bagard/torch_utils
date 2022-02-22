80c4c523dc09133cff7f47cba24d26f8703ffc4c
Hoel Bagard <hoel.bagard@gmail.com>
2022-02-22 11:40:27 +0900

    Bug fixes, removed the write_image function.

--- utils/tensorboard_template.py
+++ utils/tensorboard_template.py
@@ -1,11 +1,10 @@
-from abc import ABC, abstractmethod
+from abc import ABC
+from logging import Logger
 from pathlib import Path
-from typing import Any, Callable, Optional
+from typing import Optional
 
-import numpy as np
 import torch
 import torch.nn as nn
-from torch import Tensor
 from torch.utils.tensorboard import SummaryWriter
 
 from .batch_generator import BatchGenerator
@@ -17,60 +16,43 @@ class TensorBoard(ABC):
     def __init__(self,
                  model: nn.Module,
                  tb_dir: Path,
+                 train_dataloader: BatchGenerator,
+                 val_dataloader: BatchGenerator,
+                 logger: Logger,
                  metrics: Optional[Metrics] = None,
-                 write_graph: bool = True,
-                 input_shape: Optional[list[int]] = None):
+                 write_graph: bool = True):
         """Class with TensorBoard utility functions for classification-like tasks.
 
         Args:
             model (nn.Module): Pytorch model whose performance are to be recorded
             tb_dir (Path): Path to where the tensorboard files will be saved
+            train_dataloader (BatchGenerator): DataLoader with a PyTorch DataLoader like interface, contains train data
+            val_dataloader (BatchGenerator): DataLoader containing  validation data
+            logger (Logger): Used to print things.
             metrics (Metrics, optional): Instance of the Metrics class, used to compute classification metrics
             write_graph (bool): If True, add the network graph to the TensorBoard
-            input_shape (list, optional): Shape of a sample. Must be given if adding the network graph.
         """
         super().__init__()
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = model
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
         self.metrics = metrics
+        self.logger = logger
 
         self.weights_warning_printed: bool = False  # Prints a warning if the network cannot give its weights
 
         self.train_tb_writer = SummaryWriter(tb_dir / "Train")
         self.val_tb_writer = SummaryWriter(tb_dir / "Validation")
         if write_graph:
-            print("Adding network graph to TensorBoard")
-            assert input_shape is not None, "You must provide the input shape to get the network graph"
-            dummy_input = (torch.empty(2, *input_shape, device=self.device), )
+            self.logger.info("Adding network graph to TensorBoard")
+            dummy_input = (torch.empty(2, *self.train_dataloader.data_shape, device=self.device), )
             self.train_tb_writer.add_graph(model, dummy_input)
             self.train_tb_writer.flush()
 
     def close_writers(self) -> None:
         self.train_tb_writer.close()
         self.val_tb_writer.close()
-
-    @abstractmethod
-    def write_images(self,
-                     epoch: int,
-                     dataloader: BatchGenerator,
-                     draw_fn: Callable[[Tensor, Tensor, Tensor, Optional[Any]], np.ndarray],
-                     mode: str = "Train",
-                     preprocess_fn: Optional[Callable[["TensorBoard", Tensor, Tensor], tuple[Tensor, Tensor]]] = None,
-                     postprocess_fn: Optional[Callable[["TensorBoard", Tensor, Tensor], tuple[Tensor, Tensor]]] = None):
-        """Writes images with predictions written on them to TensorBoard.
-
-        Args:
-            epoch (int): Current epoch
-            dataloader (BatchGenerator): The images will be sampled from this dataset
-            draw_fn (callable): Function that takes in the tensor images, labels and predictions
-                                and draws on the images before returning them.
-            mode (str): Either "Train" or "Validation"
-            preprocess_fn (callable, optional): Function called before inference.
-                                                Gets data and labels as input, expects them as outputs
-            postprocess_fn (callable, optional): Function called after inference.
-                                                 Gets data and predictions as input, expects them as outputs
-        """
-        return
 
     def write_metrics(self, epoch: int, mode: str = "Train") -> None:
         """Writes metrics in TensorBoard.
